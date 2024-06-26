df9ccf64752541b56abfc95c92287b96b6deaab6
AdlerCastro <adlercastro01@gmail.com>
2024-06-26 17:55:53 -0300

    :sparkles: feat: Componente Hero

--- website-pessoal/src/components/templates/Hero/index.tsx
+++ website-pessoal/src/components/templates/Hero/index.tsx
@@ -1,15 +1,30 @@
 import { sections } from "@/constants/sections"
+import Image from "next/image"
+import Nuvens from 'public/images/Nuvens.jpg'
+import { IoMenuOutline } from "react-icons/io5";
+
+//Components
+import ButtonNav from "./components/ButtonNav"
+
+//Styles
 import './style.css'
 
 const Hero = () => {
-    const style = {
-        fontSize: 'small',
-    };
 
   return (
-    <section id={sections.hero} className="footer">
-        <p>Teste</p>
-        <p style={style}>Desenvolvido por Adler Castro</p>
+    <section id={sections.hero} className="hero">
+      <Image id="image" src={Nuvens} alt={"Imagem Hero"} />
+      <div id="Menu">
+        <header>
+          <ButtonNav><IoMenuOutline /></ButtonNav>
+        </header>
+        <div className="LinksNav">
+          <a id="links" href="https://google.com" target="_blank">Google</a>
+          <a id="links" href="https://google.com" target="_blank">Google</a>
+          <a id="links" href="https://google.com" target="_blank">Google</a>
+          <a id="links" href="https://google.com" target="_blank">Google</a>
+        </div>
+      </div>
     </section>
   )
 }
