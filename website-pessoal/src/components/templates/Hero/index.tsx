d5465cadc5ad6b015d4aa90d4c979955c2b09c1e
AdlerCastro <adlercastro01@gmail.com>
2024-06-26 12:01:34 -0300

    :sparkles: Componente Hero

---
+++ website-pessoal/src/components/templates/Hero/index.tsx
@@ -0,0 +1,12 @@
+import { sections } from "@/constants/sections"
+import './style.css'
+
+const Hero = () => {
+  return (
+    <section id={sections.hero} className="footer">
+        <h1>Hero</h1>
+    </section>
+  )
+}
+
+export default Hero
---
+++ website-pessoal/src/components/templates/Hero/stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react";
+import Hero from ".";
+
+export default{
+    title: 'Components/Templates/Hero',
+    component: Hero
+}as Meta<StoryObj>
+
+type Story = StoryObj;
+
+export const Template:Story = {}
