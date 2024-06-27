e93c99f308495bf719f3fcd6b24e56b4563510e6
AdlerCastro <adlercastro01@gmail.com>
2024-06-27 13:47:44 -0300

    :construction: Em andamento

--- website-pessoal/src/app/page.tsx
+++ website-pessoal/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/templates/Hero";
 import Image from "next/image";
+import Header from "@/components/organisms/Header";
 
 export default function Home() {
   return (
     <>
-      <Hero/>
+      <Header/>
+      {/* <Hero/> */}
     </>
   );
 }
--- website-pessoal/src/components/templates/Hero/index.tsx
+++ website-pessoal/src/components/templates/Hero/index.tsx
@@ -1,10 +1,4 @@
 import { sections } from "@/constants/sections"
-import Image from "next/image"
-import Nuvens from 'public/images/Nuvens.jpg'
-import { IoMenuOutline } from "react-icons/io5";
-
-//Components
-import ButtonNav from "./components/ButtonNav"
 
 //Styles
 import './style.css'
@@ -13,18 +7,7 @@ const Hero = () => {
 
   return (
     <section id={sections.hero} className="hero">
-      <Image id="image" src={Nuvens} alt={"Imagem Hero"} />
-      <div id="Menu">
-        <header>
-          <ButtonNav><IoMenuOutline /></ButtonNav>
-        </header>
-        <div className="LinksNav">
-          <a id="links" href="https://google.com" target="_blank">Google</a>
-          <a id="links" href="https://google.com" target="_blank">Google</a>
-          <a id="links" href="https://google.com" target="_blank">Google</a>
-          <a id="links" href="https://google.com" target="_blank">Google</a>
-        </div>
-      </div>
+
     </section>
   )
 }
