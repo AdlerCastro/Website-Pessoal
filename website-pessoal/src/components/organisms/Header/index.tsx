7a7977312615e2062049d12b803a68021ca8663d
AdlerCastro <adlercastro01@gmail.com>
2024-06-27 17:47:09 -0300

    :lipstick: feat: Estilização do menu nav mobile

--- website-pessoal/src/components/organisms/Header/index.tsx
+++ website-pessoal/src/components/organisms/Header/index.tsx
@@ -1,24 +1,47 @@
+'use client'
+
 import { IoMenuOutline } from "react-icons/io5";
 // import { IoIosClose } from "react-icons/io";
-import Image from "next/image";
 import Eu from 'public/images/Eu.jpg'
-import "./styles.css"
+
+// Imports
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+// Components
 import ButtonNav from "./components/ButtonNav";
-import "./scripts/script"
+// Styles
+import "./styles.css"
 
 const Header = () => {
-    
+
+    const[active, setActive] = useState(false);
+
+    const toggleActive = () => {
+        return(
+            setActive(!active)
+        )
+    };
+
     return (
         <header className="Header">
             <nav className="Nav">
-                <a><Image src={Eu} alt="Foto Adler Castro" id="image"/></a>
-                <ButtonNav id="ButtonNav"><IoMenuOutline /></ButtonNav>
+                <a className="LinkImage"><Image src={Eu} alt="Foto Adler Castro" id="image" /></a>
+                <ButtonNav onClick={toggleActive} id="ButtonNav"><IoMenuOutline /></ButtonNav>
                 <ul className="Nav-list">
-                    <li><a href="https://google.com" target="_blank">Sobre</a></li>
-                    <li><a href="https://google.com" target="_blank">Projetos</a></li>
-                    <li><a href="https://google.com" target="_blank">Contatos</a></li>
+                    <li><Link href="https://google.com" target="_blank">Sobre</Link></li>
+                    <li><Link href="https://google.com" target="_blank">Projetos</Link></li>
+                    <li><Link href="https://google.com" target="_blank">Contatos</Link></li>
                 </ul>
             </nav>
+            {active && (
+                <ul className="Nav-Mobile">
+                    <li><Link href="https://google.com" target="_blank">Sobre</Link></li>
+                    <li><Link href="https://google.com" target="_blank">Projetos</Link></li>
+                    <li><Link href="https://google.com" target="_blank">Contatos</Link></li>
+                </ul>
+            )}
         </header>
     )
 }
