7c60e9018e64743bd11540be96174d9bc95c8538
AdlerCastro <adlercastro01@gmail.com>
2024-06-28 12:25:09 -0300

    :lipstick: style: Estilizando o menu

--- website-pessoal/src/components/organisms/Header/index.tsx
+++ website-pessoal/src/components/organisms/Header/index.tsx
@@ -7,41 +7,47 @@ import Eu from 'public/images/Eu.jpg'
 // Imports
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from "react";
+// import { useState } from "react";
 
 // Components
-import ButtonNav from "./components/ButtonNav";
+// import ButtonNav from "./components/ButtonNav";
 // Styles
 import "./styles.css"
 
 const Header = () => {
 
-    const[active, setActive] = useState(false);
+    // const [active, setActive] = useState(false);
 
-    const toggleActive = () => {
-        return(
-            setActive(!active)
-        )
-    };
+    // const toggleActive = () => {
+    //     return (
+    //         setActive(!active)
+    //     )
+    // };
 
     return (
-        <header className="Header">
-            <nav className="Nav">
-                <a className="LinkImage"><Image src={Eu} alt="Foto Adler Castro" id="image" /></a>
-                <ButtonNav onClick={toggleActive} id="ButtonNav"><IoMenuOutline /></ButtonNav>
-                <ul className="Nav-list">
-                    <li><Link href="https://google.com" target="_blank">Sobre</Link></li>
-                    <li><Link href="https://google.com" target="_blank">Projetos</Link></li>
-                    <li><Link href="https://google.com" target="_blank">Contatos</Link></li>
+        <header className="header">
+            <nav className="nav">
+                <a className="LinkImage"><Image src={Eu} alt="Foto Adler Castro" id="image"/></a>
+                <input type="checkbox" className='checkbox' id='checkbox' />
+                <label htmlFor="checkbox" className='label-menu'>
+                    <IoMenuOutline />
+                </label>
+                {/* <ButtonNav onClick={toggleActive} id="ButtonNav"><IoMenuOutline /></ButtonNav> */}
+                <ul className="menu">
+                    <div className="menu-list">
+                        <li><Link href="https://google.com" target="_blank">Sobre</Link></li>
+                        <li><Link href="https://google.com" target="_blank">Projetos</Link></li>
+                        <li><Link href="https://google.com" target="_blank">Contatos</Link></li>
+                    </div>
                 </ul>
             </nav>
-            {active && (
+            {/* {active && (
                 <ul className="Nav-Mobile">
                     <li><Link href="https://google.com" target="_blank">Sobre</Link></li>
                     <li><Link href="https://google.com" target="_blank">Projetos</Link></li>
                     <li><Link href="https://google.com" target="_blank">Contatos</Link></li>
                 </ul>
-            )}
+            )} */}
         </header>
     )
 }
