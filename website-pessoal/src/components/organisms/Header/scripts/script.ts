18a4365d8fbc649c3b56fe7adb2525a88319742a
AdlerCastro <adlercastro01@gmail.com>
2024-06-28 11:46:55 -0300

    :fire: Arquivo desnecessário

--- website-pessoal/src/components/organisms/Header/scripts/script.ts
+++
@@ -1,6 +0,0 @@
-
-
-// const ButtonNav = document.querySelector("#ButtonNav");
-// const Nav = document.querySelector(".Nav");
-
-// ButtonNav?.addEventListener("click", () => Nav?.classList.toggle("active"));
