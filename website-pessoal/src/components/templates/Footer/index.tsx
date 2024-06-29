3b2a7ee4c8aa574553f4e45472d9476f339ca462
AdlerCastro <adlercastro01@gmail.com>
2024-06-29 20:06:20 -0300

    :construction: Desenvolvendo o Footer

--- website-pessoal/src/components/templates/Footer/index.tsx
+++ website-pessoal/src/components/templates/Footer/index.tsx
@@ -1,15 +1,32 @@
 import { sections } from '@/constants/sections';
+import Image from 'next/image';
+import Link from '@/components/atoms/Link';
+
+import GithubIcon from 'public/icons/GithubIcon.svg'
+import EmailIcon from 'public/icons/EmailIcon.svg'
+import InstagramIcon from 'public/icons/InstagramIcon.svg'
+import LinkedInIcon from 'public/icons/LinkedInIcon.svg'
+import { MdWhatsapp } from "react-icons/md";
+
 import './style.css';
 
 const Footer = () => {
-    const style = {
-        fontSize: "small",
-    }
+  const style = {
+    fontSize: "medium",
+  }
 
   return (
-    <section id={sections.footer} className='footer'>
-        <p>Teste</p>
-        <p style={style}>Desenvolvido por Adler Castro</p>
+    <section id={sections.Footer} className='footer'>
+      <div className='Contatos'>
+        <h1>Contacts</h1>
+        <div className='SocialMedia'>
+          <Link href='https://www.instagram.com/adler_castro/' target='_blank'><Image src={InstagramIcon} alt='Instagram' /></Link>
+          <Link href='https://www.linkedin.com/in/adler-augustus-de-castro-mota/' target='_blank'><Image src={LinkedInIcon} alt='LinkedIn' /></Link>
+          <Link id='whatsapp' target='_blank'><MdWhatsapp /></Link>
+          <Link target='_blank'><Image src={EmailIcon} alt='Email' /></Link>
+        </div>
+      </div>
+      <p style={style}>Desenvolvido por Adler Castro</p>
     </section>
   )
 }
