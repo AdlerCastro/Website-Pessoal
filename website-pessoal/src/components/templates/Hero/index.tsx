cc44219174f9797b7e7a6479ea983677cec218cf
AdlerCastro <adlercastro01@gmail.com>
2024-06-30 00:18:34 -0300

    :lipstick: style: Estilizando o Hero

--- website-pessoal/src/components/templates/Hero/index.tsx
+++ website-pessoal/src/components/templates/Hero/index.tsx
@@ -2,12 +2,17 @@ import { sections } from "@/constants/sections"
 
 //Styles
 import './style.css'
+import Header from "@/components/organisms/Header"
 
 const Hero = () => {
 
   return (
     <section id={sections.Hero} className="hero">
-      
+      <main className="bodyHero">
+        <h1>Bem vindo ao meu Website</h1>
+        <p>Apresentarei algumas informações sobre mim a respeito do lado profissional e pessoal <del>ou não</del></p>
+      </main>
+      <Header/>
     </section>
   )
 }
