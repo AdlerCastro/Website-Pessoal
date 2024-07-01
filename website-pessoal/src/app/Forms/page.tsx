52ca6d62f4c188bb6770d4509ed52070c3d08927
AdlerCastro <adlercastro01@gmail.com>
2024-07-01 13:08:36 -0300

    :sparkles: feat: Formulário com integração ao Gmail

---
+++ website-pessoal/src/app/Forms/page.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import Header from '@/components/organisms/Header'
+import './globals.css'
+
+import { useState } from 'react'
+import emailjs from '@emailjs/browser'
+
+export default function Forms(){
+
+    const[name, setName] = useState('');
+    const[email, setEmail] = useState('');
+    const[message, setMessage] = useState('');
+
+    function sendEmail(e: { preventDefault: () => void; }){
+        e.preventDefault();
+
+        if(name === '' || email === '' || message === ''){
+            alert("Preencha todos os campos");
+            return;
+        }
+
+        const templateParams = {
+            from_name: name,
+            message: message,
+            email: email,
+        }
+
+        emailjs.send("service_scog29q", "template_wr7r5hh", templateParams, "k6fNu668XWG5IyQWU").then((response) =>{
+            console.log("EMAIL ENVIADO", response.status, response.text)
+            setName('')
+            setEmail('')
+            setMessage('')
+        }, (err) =>{
+            console.log("ERRO: ", err)
+        })
+    }
+
+    return(
+        <div className='body'>
+            <Header/>
+            <div className='spaceForm'>
+                <form className="form" onSubmit={sendEmail} >
+                    <input
+                        type="text" 
+                        placeholder='Nome' 
+                        onChange={(e) => {setName(e.target.value)}}
+                        value={name}/>
+                    <input
+                        type="email"
+                        placeholder='Email'
+                        onChange={(e) => {setEmail(e.target.value)}}
+                        value={email}/>
+                    <textarea
+                        className="textarea"
+                        placeholder='Digite sua mensagem'
+                        onChange={(e) => {setMessage(e.target.value)}}
+                        value={message}
+                    ></textarea>
+                    <input className='button' type="submit" placeholder='Enviar' />
+                </form>
+            </div>
+        </div>
+    )
+}
