92d8fdf070a084f8b80a609d440cd76cc4c9bd27
AdlerCastro <adlercastro01@gmail.com>
2024-07-01 17:41:30 -0300

    :construction: Em progresso

--- website-pessoal/src/app/page.tsx
+++ website-pessoal/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/templates/Hero";
 import About from "@/components/templates/About";
+import Projects from "@/components/templates/Projects";
 
 export default function Home() {
   return (
     <>
       <Hero/>
       <About/>
+      <Projects/>
     </>
   );
 }
