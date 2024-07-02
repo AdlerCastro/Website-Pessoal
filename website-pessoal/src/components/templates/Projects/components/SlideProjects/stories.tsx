63e37cb98941d3f1791deb2f2d0e7a62f4e0c63f
AdlerCastro <adlercastro01@gmail.com>
2024-07-02 17:44:30 -0300

    :sparkles: feat: Slide dos projetos

---
+++ website-pessoal/src/components/templates/Projects/components/SlideProjects/stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react"
+import SlideProjects from "."
+
+export default{
+    title: "Components/Templates/Projects/Components/SlideProjects",
+    component: SlideProjects
+}as Meta<StoryObj>
+
+type Story = StoryObj;
+
+export const Default:Story = {}
