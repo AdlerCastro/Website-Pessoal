d26a9604cec3cc0651d8d1774d8c5a84ceecf898
AdlerCastro <adlercastro01@gmail.com>
2024-07-03 14:51:20 -0300

    :lipstick: style: Ajustando o CSS da seção

--- website-pessoal/src/components/templates/About/index.tsx
+++ website-pessoal/src/components/templates/About/index.tsx
@@ -7,13 +7,28 @@ const About = () => {
   return (
     <>
       <section id={sections.About} className="about">
+
         <div className="description">
           <h1>Quem é Adler Castro?</h1>
           <p>Cursando Engenharia de Computação no Centro Universitário do Estado do Pará, graduando do 3° semestre, com experiências em desenvolvimento de software front-end. Designado a Diretor de comunicações públicas do Centro Acadêmico de Engenharia de Computação decorrente as habilidades de oratória, comunicação, dedicação, e experiências de trabalho em equipe relacionadas a gestão, liderança e desempenho, observadas ao longo da carreira universitária.</p>
 
           <p>Conhecimento técnico e habilidades adquiridos em experiências no Escritório de Engenharia do Cesupa, grupo de pesquisa no qual habilidades profissionais na área de programação, eletrônica e embarcados são desenvolvidas.</p>
+
           <p>Além disso, sou membro do Gamelab, grupo universitário de estudo e pesquisa que possibilita a prática e vivência de técnicas de programação na área de Desenvolvimento de Jogos Digitais e soluções gamificadas.</p>
         </div>
+
+        <div className="Skills">
+          <h1>Principais Competências</h1>
+          <ul className="listSkills">
+            <li>Em processo de formação front-end e react/react native pela Udemy</li>
+            <li>Conhecimento aplicado de utilizando Microsoft 365: Excel, Word, PowerPoint</li>
+            <li>Conhecimento aplicado em modelagem 3D no SolidWorks</li>
+            <li>Conhecimento aplicado em eletrônica e sistemas embarcados</li>
+            <li>Em processo de formação em Node.js pela Udemy</li>
+            <li>Metodologia ágil &#40;Scrum, Kanban&#41;</li>
+          </ul>
+        </div>
+
         <Image src={Eu} alt="Foto de Perfil" className="image" />
       </section>
     </>
