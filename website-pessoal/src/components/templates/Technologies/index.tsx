88cff45485b549b025ff76c196c2b1b57b5b8082
AdlerCastro <adlercastro01@gmail.com>
2024-07-04 11:24:42 -0300

    :sparkles: feat: Seçãao Technologies

---
+++ website-pessoal/src/components/templates/Technologies/index.tsx
@@ -0,0 +1,12 @@
+import { sections } from '@/constants/sections'
+import './styles.css'
+
+const Technologies = () => {
+  return (
+    <div id={sections.Technologies} className='bodyTechnologies'>
+        <h1>Tecnologias utilizadas</h1>
+    </div>
+  )
+}
+
+export default Technologies
---
+++ website-pessoal/src/components/templates/Technologies/stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from "@storybook/react"
+import Technologies from "."
+
+export default{
+    title: "Components/Templates/Technologies",
+    component: Technologies
+}as Meta<StoryObj>
+
+type Story = StoryObj
+
+export const Default:Story = {}
