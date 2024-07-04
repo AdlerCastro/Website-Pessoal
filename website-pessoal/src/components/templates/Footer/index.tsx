a88d24f82460709bd075252aebc4460cc65fc514
AdlerCastro <adlercastro01@gmail.com>
2024-07-04 16:27:44 -0300

    :ok_hand: style: Ajustando CSS da seção

--- website-pessoal/src/components/templates/Footer/index.tsx
+++ website-pessoal/src/components/templates/Footer/index.tsx
@@ -18,16 +18,17 @@ const Footer = () => {
   return (
     <section id={sections.Contacts} className='footer'>
       <div className='Contatos'>
-        <h1>Contacts</h1>
+        <div className='Subscribe'>
+          <h1>Acompanhe-me no Github para conhecer mais projetos desenvolvidos</h1>
+          <NextLink className='GithubButton' href='https://github.com/AdlerCastro' target='_blank'><Image className='iconGithub' src={GithubIcon} alt='Github' /></NextLink>
+        </div>
+        <h1>Entre em contato por:</h1>
         <div className='SocialMedia'>
           <SocialMediaButton className='SocialMediaButtons' href='https://www.instagram.com/adler_castro/' target='_blank'><Image className='icons' src={InstagramIcon} alt='Instagram' /></SocialMediaButton>
 
           <SocialMediaButton className='SocialMediaButtons' href='https://www.linkedin.com/in/adler-augustus-de-castro-mota/' target='_blank'><Image className='icons' src={LinkedInIcon} alt='LinkedIn' /></SocialMediaButton>
-          
-          <NextLink className='SocialMediaButtons' href='https://github.com/AdlerCastro' target='_blank'><Image className='icons' src={GithubIcon} alt='Github' /></NextLink>
 
           <NextLink href='/Forms' className='SocialMediaButtons'><Image className='icons' src={EmailIcon} alt='Email' /></NextLink>
-
 
         </div>
       </div>
