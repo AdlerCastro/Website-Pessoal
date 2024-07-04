73cfad870ada58511406d593a53d4b9bec30bb75
AdlerCastro <adlercastro01@gmail.com>
2024-07-04 11:24:54 -0300

    :construction: Concluindo

--- website-pessoal/src/app/page.tsx
+++ website-pessoal/src/app/page.tsx
@@ -1,12 +1,14 @@
 import Hero from "@/components/templates/Hero";
 import About from "@/components/templates/About";
 import Projects from "@/components/templates/Projects";
+import Technologies from "@/components/templates/Technologies";
 
 export default function Home() {
   return (
     <>
       <Hero/>
       <About/>
+      <Technologies />
       <Projects/>
     </>
   );
