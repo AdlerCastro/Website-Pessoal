de83f459da3150fcb7e3009887cca753a6a81e27
AdlerCastro <adlercastro01@gmail.com>
2024-07-05 16:04:12 -0300

    :lipstick: style: Adicionando a fonte Poppins

--- website-pessoal/src/app/layout.tsx
+++ website-pessoal/src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Footer from "@/components/templates/Footer";
+import  { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  weight: '400',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
   title: "Forum: Adler Castro",
@@ -13,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={poppins.className}>
       <body>{children}</body>
       <Footer/>
     </html>
