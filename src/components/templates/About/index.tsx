611c8a90b411c90ba4822f904167eb00aeb1ff68
AdlerCastro <adlercastro01@gmail.com>
2024-09-08 13:39:10 -0300

    :lipstick: feat: Estilizando sessão responsiva

--- src/components/templates/About/index.tsx
+++ src/components/templates/About/index.tsx
@@ -1,7 +1,7 @@
 import { sections } from "@/constants/sections";
 import Image from "next/image";
 import Eu from "public/images/Eu.jpg";
-import "./styles.css";
+// import "./styles.css";
 
 export default function About() {
   return (
@@ -11,58 +11,86 @@ export default function About() {
         className="w-full flex flex-col items-center justify-center bg-about text-white py-10"
       >
         <div className="flex flex-col gap-3">
-          <div className="flex flex-col gap-4 m-2 p-8 bg-default rounded-xl">
-            <h2 className="text-start text-2xl mb-2">Quem é Adler Castro?</h2>
-            <div className="flex flex-col gap-4 justify-center flex-wrap">
-              <p>
-                Cursando Engenharia de Computação no Centro Universitário do
-                Estado do Pará, graduando do 3° semestre, com experiências em
-                desenvolvimento de software front-end. Designado a Diretor de
-                comunicações públicas do Centro Acadêmico de Engenharia de
-                Computação decorrente as habilidades de oratória, comunicação,
-                dedicação, e experiências de trabalho em equipe relacionadas a
-                gestão, liderança e desempenho, observadas ao longo da carreira
-                universitária.
-              </p>
+          <div className="flex flex-col gap-4 m-2 p-8">
+            <Image
+              src={Eu}
+              alt="Foto de Perfil"
+              className="w-[300px] h-[300px] rounded-full transition-all duration-300 ease-in-out p-5 block lg:hidden self-center"
+            />
+            <h2 className="text-center sm:text-start text-2xl mb-2">
+              Quem é Adler Castro?
+            </h2>
+            <div className="flex flex-row gap-4 lg:gap-8">
+              <div className="flex flex-1 flex-col gap-4">
+                <p className="text-start bg-default rounded-xl p-4 border border-solid border-paragraph max-w-paragraph hover:scale-105 transition-all duration-300">
+                  Cursando Engenharia de Computação no Centro Universitário do
+                  Estado do Pará, graduando do 4° semestre, com experiências em
+                  desenvolvimento de software front-end.
+                </p>
 
-              <p>
-                Conhecimento técnico e habilidades adquiridos em experiências no
-                Escritório de Engenharia do Cesupa, grupo de pesquisa no qual
-                habilidades profissionais na área de programação, eletrônica e
-                embarcados são desenvolvidas.
-              </p>
+                <p className="text-start bg-default rounded-xl p-4 border border-solid border-paragraph max-w-paragraph hover:scale-105 transition-all duration-300">
+                  Designado a Diretor de comunicações públicas do Centro
+                  Acadêmico de Engenharia de Computação decorrente as
+                  habilidades de oratória, comunicação, dedicação, e
+                  experiências de trabalho em equipe relacionadas a gestão,
+                  liderança e desempenho, observadas ao longo da carreira
+                  universitária.
+                </p>
 
-              <p>
-                Além disso, sou membro do Gamelab, grupo universitário de estudo
-                e pesquisa que possibilita a prática e vivência de técnicas de
-                programação na área de Desenvolvimento de Jogos Digitais e
-                soluções gamificadas.
-              </p>
+                <p className="text-start bg-default rounded-xl p-4 border border-solid border-paragraph max-w-paragraph hover:scale-105 transition-all duration-300">
+                  Conhecimento técnico e habilidades adquiridos em experiências
+                  no Escritório de Engenharia do Cesupa, grupo de pesquisa no
+                  qual habilidades profissionais na área de programação,
+                  eletrônica e embarcados são desenvolvidas.
+                </p>
+
+                <p className="text-start bg-default rounded-xl p-4 border border-solid border-paragraph max-w-paragraph hover:scale-105 transition-all duration-300">
+                  Além disso, sou membro do Gamelab, grupo universitário de
+                  estudo e pesquisa que possibilita a prática e vivência de
+                  técnicas de programação na área de Desenvolvimento de Jogos
+                  Digitais e soluções gamificadas.
+                </p>
+              </div>
+              <div className="relative w-[350px] ml-5 group/profile p-5">
+                <div className="w-[300px] h-[300px] group-hover/profile:w-[200px] group-hover/profile:h-[200px] rounded-full absolute inset-0 mx-auto mt-5 transition-all duration-500 ease-in-out hidden lg:block z-[2]">
+                  <Image
+                    src={Eu}
+                    alt="Foto de Perfil"
+                    className="w-full h-full rounded-full group-hover/profile:border group-hover/profile:border-solid group-hover/profile:border-paragraph"
+                  />
+                </div>
+                <div className="w-full h-0 overflow-hidden bg-zinc-800 flex justify-center items-end absolute inset-0 group-hover/profile:h-full transition-all ease-in-out duration-300 rounded-lg">
+                  <p>Funciona</p>
+                </div>
+              </div>
             </div>
           </div>
           <div className="flex flex-col p-7 m-2 gap-4">
-            <h2 className="text-start mb-2">Principais Competências</h2>
-            <ul className="grid grid-cols-2 gap-4">
-              <li>
+            <h2 className="text-start mb-2 text-xl">Principais Competências</h2>
+            <ul className="grid grid-cols-2 gap-4 text-center text-sm">
+              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
                 Em processo de formação front-end e react/react native pela
                 Udemy
               </li>
-              <li>
+              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
                 Conhecimento aplicado de utilizando Microsoft 365: Excel, Word,
                 PowerPoint
               </li>
-              <li>Conhecimento aplicado em modelagem 3D no SolidWorks</li>
-              <li>Conhecimento aplicado em eletrônica e sistemas embarcados</li>
-              <li>Em processo de formação em Node.js pela Udemy</li>
-              <li>Metodologia ágil &#40;Scrum, Kanban&#41;</li>
+              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
+                Conhecimento aplicado em modelagem 3D no SolidWorks
+              </li>
+              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
+                Conhecimento aplicado em eletrônica e sistemas embarcados
+              </li>
+              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
+                Em processo de formação em Node.js pela Udemy
+              </li>
+              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
+                Metodologia ágil &#40;Scrum, Kanban&#41;
+              </li>
             </ul>
           </div>
         </div>
-        <Image
-          src={Eu}
-          alt="Foto de Perfil"
-          className="w-[300px] h-[300px] rounded-full transition-all duration-300 ease-in-out p-5 mr-5"
-        />
       </div>
     </>
   );
