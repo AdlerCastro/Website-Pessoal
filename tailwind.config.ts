b0dcbb9eb8974422e3055eed4eaf8491a8da5128
AdlerCastro <adlercastro01@gmail.com>
2024-09-08 03:37:09 -0300

    :wrench: chore: Definindo variáveis de estilos

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from "tailwindcss";
+
+const config:Config = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      backgroundColor: {
+        default: "var(--background-color-default)",
+      },
+      backgroundImage: {
+        about: "linear-gradient(180deg, #2083bd, #09203F 50%)"
+      },
+      gridTemplateColumns: {
+        dashboard: "300px 1fr",
+      },
+      fontSize: {
+        "2xs": "0.625rem",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
+
+export default config;
