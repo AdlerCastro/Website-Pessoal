7006037a17fb64b60d0ad2a7f0d801df50992edd
AdlerCastro <adlercastro01@gmail.com>
2024-09-08 16:34:09 -0300

    :lipstick: feat: Componente hero com tailwindcss

--- src/components/templates/Hero/index.tsx
+++ src/components/templates/Hero/index.tsx
@@ -1,19 +1,25 @@
-import { sections } from "@/constants/sections"
-
-//Styles
-import './style.css'
-
+import { sections } from "@/constants/sections";
+import { BsArrowDown } from "react-icons/bs";
 
 const Hero = () => {
-
   return (
-    <div id={sections.Hero} className="hero">
-      <div className="bodyHero">
-        <h1>Bem vindo</h1>
-        <p>Apresentarei algumas informações sobre mim a respeito do lado profissional e pessoal <del>ou não</del></p>
+    <div
+      id={sections.Hero}
+      className="w-full h-screen flex flex-col items-center justify-center bg-hero bg-sizeHero animate-bgHero transition-all duration-100"
+    >
+      <div className="flex-1 flex flex-col gap-4 items-center justify-center">
+        <h1 className="text-5xl md:text-6xl lg:text-7xl xl:text-8xl">
+          Bem vindo
+        </h1>
+        <p className="px-4 md:px-8 text-center text-base ">
+          Apresentarei algumas informações sobre mim a respeito do lado
+          profissional e pessoal <del>ou não</del>
+        </p>
       </div>
-    </div>
-  )
-}
 
-export default Hero
+      <BsArrowDown size={20} className="animate-bounce" />
+    </div>
+  );
+};
+
+export default Hero;
