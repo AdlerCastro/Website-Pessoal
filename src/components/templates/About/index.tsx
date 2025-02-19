52061dc5896527af430cc5ae7e93c8be837c72f9
Adler Castro <adlercastro01@gmail.com>
2025-02-19 01:01:33 -0300

    :recycle: refactor: Organizando a seção about

--- src/components/templates/About/index.tsx
+++ src/components/templates/About/index.tsx
@@ -1,96 +1,135 @@
-import { SECTIONS } from "@/enums/sections.enum";
-import Image from "next/image";
-import Eu from "public/images/Eu.jpg";
+'use client';
+
+import { Typograph } from '@/components/atoms/typograph';
+import { ABOUT } from '@/constants/info/about';
+import { SECTIONS } from '@/enums/sections.enum';
+import { useDivideArray } from '@/hooks/use-divide-array.hook';
+import { cn } from '@/lib/utils';
+import { AnimatedElementsTypes, HandleObserver } from '@/utils/scrollAnims';
+import Image from 'next/image';
+import Eu from 'public/images/Eu.jpg';
+import { useRef, useState } from 'react';
 
 export default function About() {
+  const [isVisible, setIsVisible] = useState<boolean[]>(Array(5).fill(false));
+
+  const animatedElementsRef = useRef<(AnimatedElementsTypes | null)[]>(
+    Array(5).fill(null),
+  );
+
+  HandleObserver({
+    setIsVisible,
+    isVisible,
+    animatedElementsRef,
+  });
+
+  const { firstHalf, secondHalf } = useDivideArray({
+    array: ABOUT.competences,
+  });
+
   return (
-    <>
-      <div
-        id={SECTIONS.About}
-        className="w-full flex flex-col items-center justify-center bg-about text-white py-10"
-      >
-        <div className="flex flex-col gap-3">
-          <div className="flex flex-col gap-4 m-2 p-8">
-            <Image
-              src={Eu}
-              alt="Foto de Perfil"
-              className="w-[250px] h-[250px] rounded-full transition-all duration-300 ease-in-out p-5 block lg:hidden self-center"
-            />
-            <h2 className="text-center sm:text-start text-2xl mb-2">
-              Quem é Adler Castro?
-            </h2>
-            <div className="flex flex-row gap-4 lg:gap-8">
-              <div className="flex flex-1 flex-col gap-4">
-                <p className="text-start bg-default rounded-xl p-4 border border-solid border-paragraph max-w-paragraph hover:scale-105 transition-all duration-300">
-                  Cursando Engenharia de Computação no Centro Universitário do
-                  Estado do Pará, graduando do 4° semestre, com experiências em
-                  desenvolvimento de software front-end.
-                </p>
-
-                <p className="text-start bg-default rounded-xl p-4 border border-solid border-paragraph max-w-paragraph hover:scale-105 transition-all duration-300">
-                  Designado a Diretor de comunicações públicas do Centro
-                  Acadêmico de Engenharia de Computação decorrente as
-                  habilidades de oratória, comunicação, dedicação, e
-                  experiências de trabalho em equipe relacionadas a gestão,
-                  liderança e desempenho, observadas ao longo da carreira
-                  universitária.
-                </p>
-
-                <p className="text-start bg-default rounded-xl p-4 border border-solid border-paragraph max-w-paragraph hover:scale-105 transition-all duration-300">
-                  Conhecimento técnico e habilidades adquiridos em experiências
-                  no Escritório de Engenharia do Cesupa, grupo de pesquisa no
-                  qual habilidades profissionais na área de programação,
-                  eletrônica e embarcados são desenvolvidas.
-                </p>
-
-                <p className="text-start bg-default rounded-xl p-4 border border-solid border-paragraph max-w-paragraph hover:scale-105 transition-all duration-300">
-                  Além disso, sou membro do Gamelab, grupo universitário de
-                  estudo e pesquisa que possibilita a prática e vivência de
-                  técnicas de programação na área de Desenvolvimento de Jogos
-                  Digitais e soluções gamificadas.
-                </p>
+    <div
+      id={SECTIONS.About}
+      className='flex w-full flex-col items-center justify-center bg-about py-10 text-white'
+    >
+      <div className='flex flex-col gap-3'>
+        <div
+          ref={(el) => {
+            animatedElementsRef.current[0] = el;
+          }}
+          className={cn(
+            'm-2 flex flex-col gap-4 p-8 transition-all duration-300 ease-in-out',
+            isVisible[0]
+              ? 'translate-y-0 opacity-100'
+              : '-translate-y-10 opacity-0',
+          )}
+        >
+          <Image
+            src={Eu}
+            alt='Foto de Perfil'
+            className='block h-[250px] w-[250px] self-center rounded-full p-5 transition-all duration-300 ease-in-out lg:hidden'
+          />
+          <Typograph.Title className='text-center sm:text-start'>
+            Quem é Adler Castro?
+          </Typograph.Title>
+          <div className='flex flex-row gap-4 lg:gap-8'>
+            <div
+              ref={(el) => {
+                animatedElementsRef.current[1] = el;
+              }}
+              className={cn(
+                'flex flex-1 flex-col gap-4 transition-all duration-300 ease-in-out',
+                isVisible[1]
+                  ? 'translate-y-0 opacity-100'
+                  : '-translate-y-10 opacity-0',
+              )}
+            >
+              {ABOUT.competences.map((about, index) => (
+                <Typograph.Paragraph key={index}>{about}</Typograph.Paragraph>
+              ))}
+            </div>
+            <div
+              ref={(el) => {
+                animatedElementsRef.current[2] = el;
+              }}
+              className={cn(
+                'group/profile relative ml-5 hidden w-[350px] p-5 transition-all duration-300 ease-in-out lg:block',
+                isVisible[2]
+                  ? 'translate-y-0 opacity-100'
+                  : '-translate-y-10 opacity-0',
+              )}
+            >
+              <div className='absolute inset-0 z-[2] mx-auto mt-5 hidden h-[300px] w-[300px] rounded-full transition-all duration-500 ease-in-out group-hover/profile:h-[200px] group-hover/profile:w-[200px] lg:block'>
+                <Image
+                  src={Eu}
+                  alt='Foto de Perfil'
+                  className='h-full w-full rounded-full group-hover/profile:border group-hover/profile:border-solid group-hover/profile:border-paragraph'
+                />
               </div>
-              <div className="hidden lg:block relative w-[350px] ml-5 group/profile p-5">
-                <div className="w-[300px] h-[300px] group-hover/profile:w-[200px] group-hover/profile:h-[200px] rounded-full absolute inset-0 mx-auto mt-5 transition-all duration-500 ease-in-out hidden lg:block z-[2]">
-                  <Image
-                    src={Eu}
-                    alt="Foto de Perfil"
-                    className="w-full h-full rounded-full group-hover/profile:border group-hover/profile:border-solid group-hover/profile:border-paragraph"
-                  />
-                </div>
-                <div className="w-full h-0 overflow-hidden bg-zinc-800 flex justify-center items-end absolute inset-0 group-hover/profile:h-full transition-all ease-in-out duration-300 rounded-lg">
-                  <p>Funciona</p>
-                </div>
-              </div>
+              {/* <div className='absolute inset-0 flex h-0 w-full items-end justify-center overflow-hidden rounded-lg bg-zinc-800 transition-all duration-300 ease-in-out group-hover/profile:h-full'>
+                <p>Funciona</p>
+              </div> */}
             </div>
           </div>
-          <div className="flex flex-col p-7 m-2 gap-4">
-            <h2 className="text-start mb-2 text-xl">Principais Competências</h2>
-            <ul className="grid grid-cols-2 gap-4 text-center text-sm">
-              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
-                Em processo de formação front-end e react/react native pela
-                Udemy
-              </li>
-              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
-                Conhecimento aplicado de utilizando Microsoft 365: Excel, Word,
-                PowerPoint
-              </li>
-              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
-                Conhecimento aplicado em modelagem 3D no SolidWorks
-              </li>
-              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
-                Conhecimento aplicado em eletrônica e sistemas embarcados
-              </li>
-              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
-                Em processo de formação em Node.js pela Udemy
-              </li>
-              <li className="border border-solid border-paragraph rounded-md p-2 hover:scale-105 max-w-listItems transition-all duration-300">
-                Metodologia ágil &#40;Scrum, Kanban&#41;
-              </li>
-            </ul>
-          </div>
+        </div>
+        <div
+          ref={(el) => {
+            animatedElementsRef.current[3] = el;
+          }}
+          className={cn(
+            'm-2 flex flex-col gap-4 p-7 transition-all duration-300 ease-in-out',
+            isVisible[3]
+              ? 'translate-y-0 opacity-100'
+              : '-translate-y-10 opacity-0',
+          )}
+        >
+          <h2 className='mb-2 text-start text-xl'>Principais Competências</h2>
+          <ul className='grid grid-cols-2 gap-4 text-center text-sm'>
+            <div className='flex flex-col gap-4'>
+              {firstHalf.map((competence, index) => (
+                <Typograph.Paragraph
+                  asChild
+                  key={index}
+                  className='max-w-listItems rounded-md border border-solid border-paragraph p-2 transition-all duration-300 hover:scale-105'
+                >
+                  <li>{competence}</li>
+                </Typograph.Paragraph>
+              ))}
+            </div>
+            <div className='flex flex-col gap-4'>
+              {secondHalf.map((competence, index) => (
+                <Typograph.Paragraph
+                  asChild
+                  key={index}
+                  className='max-w-listItems rounded-md border border-solid border-paragraph p-2 transition-all duration-300 hover:scale-105'
+                >
+                  <li>{competence}</li>
+                </Typograph.Paragraph>
+              ))}
+            </div>
+          </ul>
         </div>
       </div>
-    </>
+    </div>
   );
 }
