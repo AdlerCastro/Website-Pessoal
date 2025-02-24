7e58eb004058770360955bf0a6a2f905b1fdf0c8
Adler Castro <adlercastro01@gmail.com>
2025-02-24 03:17:05 -0300

    :sparkles: feat: Adicionando seção de Contato

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,6 +2,7 @@ import Hero from '@/components/templates/Hero';
 import About from '@/components/templates/About';
 import Projects from '@/components/templates/Projects';
 import Technologies from '@/components/templates/Technologies';
+import Contact from '@/components/templates/Contact';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Technologies />
       <Projects />
+      <Contact />
     </>
   );
 }
