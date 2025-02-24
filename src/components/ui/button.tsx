4f6b84ceedbc391ec4449c3d0d6a7322aa0574f3
Adler Castro <adlercastro01@gmail.com>
2025-02-24 03:40:42 -0300

    :bug: fix: Variantes faltantes causava erro em outros componentes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,12 +9,19 @@ const buttonVariants = cva('inline-flex items-center justify-center', {
     variant: {
       default:
         'px-10 py-4 rounded-3xl bg-yellow_100 font-Copperplate text-sm font-bold transition-colors hover:bg-yellow_200 active:bg-yellow_300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-black disabled:opacity-60 disabled:pointer-events-none text-black',
+      outline:
+        'border border-zinc-200 bg-white hover:bg-zinc-100 hover:text-zinc-900 dark:border-zinc-800 dark:bg-zinc-950 dark:hover:bg-zinc-800 dark:hover:text-zinc-50',
       loading:
         'gap-10 rounded-3xl text-white bg-red_wf px-10 py-4 font-Copperplate text-sm font-bold transition-colors min-h-2 min-w-[12.375rem] disabled:opacity-60 disabled:pointer-events-none',
+    },
+    size: {
+      default: 'h-10 px-4 py-2',
+      icon: 'h-10 w-10',
     },
   },
   defaultVariants: {
     variant: 'default',
+    size: 'default',
   },
 });
 
@@ -25,12 +32,12 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
 
     return (
       <Comp
-        className={cn(buttonVariants({ variant, className }))}
+        className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
       >
