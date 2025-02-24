5d3b46e7e3afe07a4b0ee5e72c9e93a640969933
Adler Castro <adlercastro01@gmail.com>
2025-02-24 03:24:14 -0300

    :sparkles: feat: Constatne para mídias sociais

---
+++ src/constants/social-media.constant.ts
@@ -0,0 +1,17 @@
+import InstagramIcon from 'public/icons/InstagramIcon.svg';
+import LinkedInIcon from 'public/icons/LinkedInIcon.svg';
+import EmailIcon from 'public/icons/EmailIcon.svg';
+
+export const SOCIAL_MEDIA = [
+  {
+    image: InstagramIcon,
+    alt: 'Instagram',
+    link: 'https://www.instagram.com/adler_castro/',
+  },
+  {
+    image: LinkedInIcon,
+    alt: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/adler-augustus-de-castro-mota/',
+  },
+  { image: EmailIcon, alt: 'Gmail', link: 'mailto:adlercastro@gmail.com' },
+];
