33659d5ddb9e382207ae522a44a8920491741f4d
Adler Castro <adlercastro01@gmail.com>
2025-02-24 03:23:16 -0300

    :sparkles: feat: Formulário de contato

---
+++ src/components/organisms/form-contact.tsx
@@ -0,0 +1,212 @@
+'use client';
+
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormSchema, FormSchemaType } from '@/schemas/form.schema';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from '../ui/form';
+import Input from '../ui/input';
+import { ContactAction } from '@/actions/send-email/route';
+import { useEffect, useState } from 'react';
+import { Button } from '../ui/button';
+import { toast } from 'sonner';
+import { Textarea } from '../ui/textarea';
+import { useMask } from '@/hooks/use-mask';
+
+interface FormInteractsStorybook {
+  isLoading?: boolean;
+}
+
+export default function FormContact({
+  isLoading: handleSelect = false,
+}: FormInteractsStorybook) {
+  const { maskPhone } = useMask();
+  const [isLoading, setIsLoading] = useState<boolean>(handleSelect);
+
+  const form = useForm<FormSchemaType>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: '',
+      email: '',
+      number: '',
+      message: '',
+    },
+  });
+
+  useEffect(() => {
+    setIsLoading(handleSelect);
+  }, [handleSelect]);
+
+  async function onSubmit(values: FormSchemaType) {
+    try {
+      setIsLoading(true);
+
+      const response = await ContactAction(values).finally(() => {
+        setIsLoading(false);
+      });
+
+      if (response.success) {
+        toast('Formulário enviado com sucesso!', {
+          description: 'Sua mensagem foi recebida. Em breve retornaremos!',
+        });
+        setIsLoading(false);
+        form.reset();
+      } else {
+        toast('Erro ao enviar formulário', {
+          description: `Falha no envio. Por favor, entre em contato diretamente. ${response.message}`,
+        });
+        setIsLoading(false);
+      }
+    } catch (error) {
+      toast('Erro ao enviar formulário', {
+        description: `Erro ao enviar formulário. Exceção: ${error}`,
+      });
+      setIsLoading(false);
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='flex w-fit flex-col items-center gap-6 rounded-lg border border-white/20 bg-white/5 p-6'
+      >
+        <FormField
+          control={form.control}
+          name='name'
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormControl>
+                <Input
+                  required
+                  id='name'
+                  {...field}
+                  placeholder='Insira seu nome aqui'
+                  label='Nome'
+                  hasError={!!form.formState.errors.name}
+                  disabled={isLoading}
+                  onInvalid={(e) => {
+                    (e.target as HTMLInputElement).setCustomValidity(
+                      'Inserir o nome é obrigatório',
+                    );
+                  }}
+                  onInput={(e) => {
+                    (e.target as HTMLInputElement).setCustomValidity('');
+                  }}
+                />
+              </FormControl>
+              <FormMessage className='font-Roboto italic' />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='email'
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormControl>
+                <Input
+                  required
+                  id='email'
+                  {...field}
+                  placeholder='Insira seu email aqui'
+                  label='Email'
+                  hasError={!!form.formState.errors.email}
+                  disabled={isLoading}
+                  onInvalid={(e) => {
+                    (e.target as HTMLInputElement).setCustomValidity(
+                      'Inserir o email é obrigatório',
+                    );
+                  }}
+                  onInput={(e) => {
+                    (e.target as HTMLInputElement).setCustomValidity('');
+                  }}
+                />
+              </FormControl>
+              <FormMessage className='font-Roboto italic' />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='number'
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormControl>
+                <Input
+                  required
+                  id='number'
+                  type='tel'
+                  {...field}
+                  placeholder='Insira seu número aqui'
+                  label='Número'
+                  hasError={!!form.formState.errors.number}
+                  disabled={isLoading}
+                  onInvalid={(e) => {
+                    (e.target as HTMLInputElement).setCustomValidity(
+                      'Inserir o número é obrigatório',
+                    );
+                  }}
+                  onInput={(e) => {
+                    (e.target as HTMLInputElement).setCustomValidity('');
+                  }}
+                  onChange={(e) => {
+                    const maskedValue = maskPhone(e.target.value);
+                    field.onChange(maskedValue);
+                  }}
+                />
+              </FormControl>
+              <FormMessage className='font-Roboto italic' />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name='message'
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormControl>
+                <Textarea
+                  required
+                  id='message'
+                  {...field}
+                  label='Mensagem'
+                  error={!!form.formState.errors.message}
+                  placeholder='Insira sua mensagem aqui'
+                  disabled={isLoading}
+                  onInvalid={(e) => {
+                    (e.target as HTMLInputElement).setCustomValidity(
+                      'Inserir a mensagem é obrigatório',
+                    );
+                  }}
+                  onInput={(e) => {
+                    (e.target as HTMLInputElement).setCustomValidity('');
+                  }}
+                />
+              </FormControl>
+              <FormMessage className='font-Roboto italic' />
+            </FormItem>
+          )}
+        />
+        <Button
+          type='submit'
+          disabled={
+            isLoading ||
+            form.getValues().email === '' ||
+            form.getValues().message === '' ||
+            form.getValues().name === ''
+          }
+          variant={isLoading ? 'loading' : 'default'}
+          className='bg-white text-black hover:bg-white/80 active:bg-zinc-400 disabled:bg-white/60'
+        >
+          Enviar formulário
+        </Button>
+      </form>
+    </Form>
+  );
+}
