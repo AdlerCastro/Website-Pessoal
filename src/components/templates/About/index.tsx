fbbf1db1a81ea0d5c194e1a84f3efa218afbd418
Adler Castro <adlercastro01@gmail.com>
2025-03-27 09:26:38 -0300

    :sparkles: feat: Adicionnado card 3D

--- src/components/templates/About/index.tsx
+++ src/components/templates/About/index.tsx
@@ -2,6 +2,7 @@
 
 import Container from '@/components/atoms/container';
 import { Typograph } from '@/components/atoms/typograph';
+import CardParallax from '@/components/molecules/cardParallax';
 import { ABOUT } from '@/constants/info/about';
 import { SECTIONS } from '@/enums/sections.enum';
 import { useDivideArray } from '@/hooks/use-divide-array.hook';
@@ -45,11 +46,16 @@ export default function About() {
               : '-translate-y-10 opacity-0',
           )}
         >
-          <Image
+          <CardParallax
+            image={Eu}
+            alt='Foto de Perfil'
+            className='size-[250px] self-center lg:hidden'
+          />
+          {/* <Image
             src={Eu}
             alt='Foto de Perfil'
             className='block h-[250px] w-[250px] self-center rounded-full p-5 transition-all duration-300 ease-in-out lg:hidden'
-          />
+          /> */}
           <Typograph.Title className='text-center sm:text-start'>
             Quem é Adler Castro?
           </Typograph.Title>
@@ -82,13 +88,18 @@ export default function About() {
                   : '-translate-y-10 opacity-0',
               )}
             >
-              <div className='absolute inset-0 z-[2] mx-auto mt-5 hidden h-[300px] w-[300px] rounded-full transition-all duration-500 ease-in-out group-hover/profile:h-[200px] group-hover/profile:w-[200px] lg:block'>
+              <CardParallax
+                image={Eu}
+                alt='Foto de Perfil'
+                className='hidden h-[300px] w-[300px] lg:flex'
+              />
+              {/* <div className='absolute inset-0 z-[2] mx-auto mt-5 hidden h-[300px] w-[300px] rounded-full transition-all duration-500 ease-in-out group-hover/profile:h-[200px] group-hover/profile:w-[200px] lg:block'>
                 <Image
                   src={Eu}
                   alt='Foto de Perfil'
                   className='h-full w-full rounded-full group-hover/profile:border group-hover/profile:border-solid group-hover/profile:border-paragraph'
                 />
-              </div>
+              </div> */}
               {/* <div className='absolute inset-0 flex h-0 w-full items-end justify-center overflow-hidden rounded-lg bg-zinc-800 transition-all duration-300 ease-in-out group-hover/profile:h-full'>
                 <p>Funciona</p>
               </div> */}
@@ -108,23 +119,23 @@ export default function About() {
         >
           <h2 className='mb-2 text-start text-xl'>Principais Competências</h2>
           <div className='grid grid-cols-2 gap-4 text-center text-sm'>
-            <div className='flex flex-col gap-4'>
+            <div className='flex flex-col items-center gap-4'>
               {firstHalf.map((competence, index) => (
                 <Typograph.Paragraph
                   asChild
                   key={index}
-                  className='max-w-listItems rounded-md border border-solid border-paragraph p-2 transition-all duration-300 hover:scale-105'
+                  className='max-w-[30rem] rounded-md border border-solid border-paragraph p-2 transition-all duration-300 hover:scale-105'
                 >
                   <p>{competence}</p>
                 </Typograph.Paragraph>
               ))}
             </div>
-            <div className='flex flex-col gap-4'>
+            <div className='flex flex-col items-center gap-4'>
               {secondHalf.map((competence, index) => (
                 <Typograph.Paragraph
                   asChild
                   key={index}
-                  className='max-w-listItems rounded-md border border-solid border-paragraph p-2 transition-all duration-300 hover:scale-105'
+                  className='max-w-[30rem] rounded-md border border-solid border-paragraph p-2 transition-all duration-300 hover:scale-105'
                 >
                   <p>{competence}</p>
                 </Typograph.Paragraph>
